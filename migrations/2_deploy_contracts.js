a5331a411203d54b194bc167f491d79fcf086faf
Kenton Prescott <kentonprescott@gmail.com>
2018-01-21 08:22:20 -0500

    quick updates

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
-var ChainList = artifacts.require("./ChainList.sol");
+var ToastDAO = artifacts.require("./ToastDAO.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(ChainList);
+  deployer.deploy(ToastDAO);
 };
